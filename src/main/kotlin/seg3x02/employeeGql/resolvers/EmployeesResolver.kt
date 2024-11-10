d3f2bae881d8f7587c7d0dea6b63d455d1b2b6eb
dexcode2023 <dextreday@gmail.com>
2024-11-10 16:03:09 -0500

    updating employees resolver and graphqls

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -2,7 +2,34 @@ package seg3x02.employeeGql.resolvers
 
 import org.springframework.stereotype.Controller
 
-@Controller
-class EmployeesResolver {
+import com.coxautodev.graphql.tools.GraphQLQueryResolver
+import com.coxautodev.graphql.tools.GraphQLMutationResolver
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeesRepository
+import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
 
+@Controller
+class EmployeesResolver(
+    private val employeesRepository: EmployeesRepository
+) : GraphQLQueryResolver, GraphQLMutationResolver {
+
+    fun employees(): List<Employee> {
+        return employeesRepository.findAll()
+    }
+
+    fun employee(id: String): Employee? {
+        return employeesRepository.findById(id).orElse(null)
+    }
+
+    fun createEmployee(input: CreateEmployeeInput): Employee {
+        val employee = Employee(
+            name = input.name,
+            dateOfBirth = input.dateOfBirth,
+            city = input.city,
+            salary = input.salary,
+            gender = input.gender,
+            email = input.email
+        )
+        return employeesRepository.save(employee)
+    }
 }
